750a920cc0e94feed3396e8b5d028ed884401968
coreOrgSP <core.organizzazioneSP@gmail.com>
2024-10-12 14:54:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken } from "firebase/messaging";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -22,8 +15,8 @@ const firebaseConfig = {
 //const analytics = getAnalytics(app);
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
 // Initialize Firebase
 //firebase.initializeApp(firebaseConfig);
